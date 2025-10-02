b7a1b1891e560f8f7cf64794fd535835473ded20
annamironova1996 <annamironova19.96@mail.ru>
2025-10-02 21:52:48 +0700

    Добавление новых плагинов blocks-gamestore и core-gamestore

---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Plugin Name: Core General
+ * Description: Core Code for Gamestore
+ * Version: 1.0
+ * Author: Anna Mironova
+ * Author URI: @annamironova1996
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+ define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
